9fc1ce1a1db91253cd686d060164180cd72993a9
Molly Madden <molly.annette.madden@gmail.com>
2019-11-22 11:44:28 +1100

    deleted migration table

--- app/models/concerns/guestvalidator.rb
+++
@@ -1,7 +0,0 @@
-class GuestValidator < ActiveModel::Validator
-    def validate(record)
-      if !(/Anhar|Harrison|Luke|Molly|Aaron|Andrew|Chris|Christopher|Elizabeth|Liz|Heng|Izzy|Jack|Jason|Kristy|Kyle|Levente|Lev|Marco|Mark|Martyna|Max|Mel|Melissa|Michelle|Murtaza|Nhan|Pankaj|Rach|Rachael|Roddy|Samaa|Stanley|Tayla|Tess|Tessa/.match(record.body))
-        record.errors.add( :name, :guest, message: 'RSVP error: name not on pre-approved list' )
-      end
-    end
-  end
--- db/migrate/20191116113611_drop_contributions_table.rb
+++
@@ -1,9 +0,0 @@
-class DropContributionsTable < ActiveRecord::Migration[6.0]
-    def up
-      drop_table :contributions
-    end
-  
-    def down
-      raise ActiveRecord::IrreversibleMigration
-    end
-end
